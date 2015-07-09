c2c52e7108f24018cdb37196d836b68539f908d4
Tom Chapin <tchapin@gmail.com>
2015-07-09 09:52:04 -0600

    fixing issues with color distribution

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-# lifx-mikrotik-phone-activity-monitor (v0.0.3)
+# lifx-mikrotik-phone-activity-monitor (v0.0.4)
 
 1. Connects to your MikroTik router via SSH and uses the torch monitoring tool
    to identify how many VoIP phone calls currently active.
 
-2. Updates the LiFX WiFi LED bulb's colors to reflect the hue, which is determined
-   by the last part of the active phone's IP address. If more than one phone is in use,
-   it will use an average of the combined colors
+2. Updates the LiFX WiFi LED bulb's colors to show the hue, which is randomly assigned to each phone line.
+   If more than one phone is in use, it will use an average of the combined colors.
 
 ## Installation
 1. Execute```bundle install```
--- monitor_phone_router.rb
+++ monitor_phone_router.rb
@@ -6,6 +6,7 @@ require 'net/ssh'
 require 'lifx'
 require 'yaml'
 require 'fancy-open-struct'
+require 'yaml/store'
 
 puts '-------------------------------------------------------------------'
 puts 'Note: you can safely ignore any LiFX circular argument errors above'
@@ -14,21 +15,21 @@ puts '-------------------------------------------------------------------'
 class PhoneMonitor
 
   def initialize
-    @config              = {}
     @data                = ''
     @lifx_current_status = nil
     @lifx_client         = nil
     @lifx_light          = nil
     @lifx_last_updated   = Time.now
 
-    load_config
+    config
+    cache
   end
 
   def run
     init_lifx_client
 
     puts 'Connecting to Mikrotik router via ssh...'
-    Net::SSH.start(@config.mikrotik_router.ip_address, @config.mikrotik_router.ssh_user, :password => @config.mikrotik_router.ssh_pass) do |ssh|
+    Net::SSH.start(config.mikrotik_router.ip_address, config.mikrotik_router.ssh_user, :password => config.mikrotik_router.ssh_pass) do |ssh|
       channel = ssh.open_channel do |ch|
 
         pty_options = { :term       => "xterm",
@@ -38,7 +39,7 @@ class PhoneMonitor
         ch.request_pty(pty_options) do |ch_pty, pty_success|
           raise "could not establish pty" unless pty_success
 
-          ch.exec "/tool torch #{@config.mikrotik_router.monitor_interface} src-address=0.0.0.0/0 dst-address=#{@config.mikrotik_router.remote_voip_server_ip}" do |ch_cmd, cmd_success|
+          ch.exec "/tool torch #{config.mikrotik_router.monitor_interface} src-address=0.0.0.0/0 dst-address=#{config.mikrotik_router.remote_voip_server_ip}" do |ch_cmd, cmd_success|
             raise "could not execute command" unless cmd_success
 
             # "on_data" is called when the process writes something to stdout
@@ -63,12 +64,56 @@ class PhoneMonitor
     end
   end
 
-  def load_config
-    @config = if File.exists?(File.join('config.yml'))
-                FancyOpenStruct.new(::YAML.load_file(File.join('config.yml')))
-              else
-                raise 'You need to set up your config.yml file, per the README!'
-              end
+  def config
+    @config ||= if File.exists?(File.join('config.yml'))
+                  FancyOpenStruct.new(::YAML.load_file(File.join('config.yml'))).freeze
+                else
+                  raise 'You need to set up your config.yml file, per the README!'
+                end
+  end
+
+  def cache
+    @cache ||= begin
+      store = YAML::Store.new "cache.store.yml"
+      store.transaction do
+        if store[:colors].nil? || store[:colors].length != config.number_of_phone_lines
+          # Set up the block of colors by dividing the spectrum by the number of phone lines
+          store[:colors]   = []
+          separate_hues_by = 360.0 / config.number_of_phone_lines.to_f
+          current_hue      = separate_hues_by
+          config.number_of_phone_lines.times do
+            store[:colors] << {
+                hue:         current_hue,
+                ip:          nil,
+                reserved_at: nil
+            }
+            current_hue += separate_hues_by
+          end
+          store[:colors].shuffle!
+        end
+      end
+      store
+    end
+  end
+
+  def reserve_or_fetch_ip_hue(ip_address)
+    cache.transaction do
+      # Find the color reservation by ip
+      reserved_entry = cache[:colors].find { |color_data| color_data[:ip] == ip_address }
+      if reserved_entry.nil?
+        # Unable to find entry - Find the first empty reservation
+        reserved_entry = cache[:colors].find { |color_data| color_data[:ip].nil? }
+        if reserved_entry.nil?
+          # No reservations left - Replace the oldest entry
+          reserved_entry = cache[:colors].sort_by { |color_data| color_data[:reserved_at].to_i }.first
+        end
+      end
+      # Set the data
+      reserved_entry[:ip] = ip_address
+      reserved_entry[:reserved_at] = Time.now
+      # Return the reserved entry's hue
+      reserved_entry[:hue]
+    end
   end
 
   def init_lifx_client
@@ -76,10 +121,10 @@ class PhoneMonitor
     # Discover lights. Blocks until a light is found
     puts 'Looking for LiFX bulb...'
     @lifx_client.discover! do |c|
-      c.lights.with_label(@config.lifx.light.name)
+      c.lights.with_label(config.lifx.light.name)
     end
     puts '-------------------------------------------------------------------'
-    @lifx_light = @lifx_client.lights.with_label(@config.lifx.light.name)
+    @lifx_light = @lifx_client.lights.with_label(config.lifx.light.name)
   end
 
   def parse_line(line)
@@ -95,18 +140,18 @@ class PhoneMonitor
     parsed_data        = @data.scan(/ip\s*((?:[0-9]{1,3}\.){3}[0-9]{1,3})\s*(?:[0-9]{1,3}\.){3}[0-9]{1,3}\s*([0-9|\.]+)(kbps|bps|mbps)/)
     active_phone_lines = [] # Array of phone IP addresses
 
-    parsed_data.each do |data|
+    parsed_data.each do |source_ip, data_rate, rate_type|
       kilobits = 0
-      if data[2] == 'kbps'
-        kilobits = data[1].to_f
-      elsif data[2] == 'mbps'
-        kilobits = data[1].to_f / 1000
-      elsif data[2] == 'bps'
-        kilobits = data[1].to_f * 1000
+      if rate_type == 'kbps'
+        kilobits = data_rate.to_f
+      elsif rate_type == 'mbps'
+        kilobits = data_rate.to_f / 1000
+      elsif rate_type == 'bps'
+        kilobits = data_rate.to_f * 1000
       end
 
-      if kilobits >= @config.mikrotik_router.voip_activity_threshold_kilobits
-        active_phone_lines << data[0]
+      if kilobits >= config.mikrotik_router.voip_activity_threshold_kilobits
+        active_phone_lines << source_ip
       end
     end
 
@@ -114,21 +159,21 @@ class PhoneMonitor
   end
 
   def update_lifx_status(active_phone_lines)
-    if active_phone_lines.length != @lifx_current_status || (Time.now >= @lifx_last_updated + @config.lifx.periodically_refresh_delay)
+    if active_phone_lines.length != @lifx_current_status || (Time.now >= @lifx_last_updated + config.lifx.periodically_refresh_delay)
       @lifx_current_status = active_phone_lines.length
       @lifx_last_updated   = Time.now
 
-      # Determine the color, based on the suffix of each IP
-      total_hue            = active_phone_lines.reduce(0) { |sum, ip| sum + (ip.split('.').last.to_f / 255 * 360) }
-      total_hue            = total_hue / active_phone_lines.length if active_phone_lines.length > 0
-
       if active_phone_lines.length > 0
-        color = LIFX::Color.hsbk(total_hue, @config.lifx.light.saturation, @config.lifx.light.brightness, @config.lifx.light.kelvin)
+        # Determine the color as an average of the active phone line hues
+        total_hue = active_phone_lines.reduce(0) { |sum, ip| sum + reserve_or_fetch_ip_hue(ip) }
+        total_hue = total_hue / active_phone_lines.length if total_hue > 0
+        color = LIFX::Color.hsbk(total_hue, config.lifx.light.saturation, config.lifx.light.brightness, config.lifx.light.kelvin)
       else
-        color = LIFX::Color.hsbk(180, 0, 0, @config.lifx.light.kelvin)
+        # Turn light off
+        color = LIFX::Color.hsbk(180, 0, 0, config.lifx.light.kelvin)
       end
 
-      @lifx_light.set_color(color, duration: @config.lifx.fade_duration)
+      @lifx_light.set_color(color, duration: config.lifx.fade_duration)
 
       print "Phone Lines Currently Active: #{active_phone_lines.length}\r"
     end
